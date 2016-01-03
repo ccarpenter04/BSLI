893e67b68d61f519417886ea05a139307fc3a092
Christopher Carpenter <ccarpenter04@swmich.edu>
2016-01-02 23:04:17 -0500

    Added the SourceLanguage enum.

---
+++ com/carpenter/bytecode/SourceLanguage.java
@@ -0,0 +1,14 @@
+package com.carpenter.bytecode;
+//TODO Are there any other notible languages to identify?
+public enum SourceLanguage {
+  JAVA,
+  SCALA,
+  GROOVY,
+  KOTLIN,
+  //TODO
+  CLOJURE,
+  JRUBY,
+  JYTHON,
+  //DISCOVER
+  UNKNOWN;
+}
